1c2a38d8e5e70131c2931df59c895f8770317a50
DanielMoraCSG <daniel@csgroup.mx>
2016-05-06 00:13:51 -0500

    Se añade estructuda SOLID para programación orientada a objetos

---
+++ resources/views/webapp/usuario_cliente/principal/index.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DanielMora
+ * Date: 05/05/16
+ * Time: 23:56
+ */
---
+++ resources/views/webapp/usuario_socio/principal/index.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DanielMora
+ * Date: 06/05/16
+ * Time: 0:10
+ */
