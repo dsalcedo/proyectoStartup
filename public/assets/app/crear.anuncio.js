507fdcf53a5b595605aacfb4001efedd80013c89
danieluyo <dsalcedo@outlook.com>
2016-05-20 01:08:03 -0500

    #fix anuncios mapa se corrige bus que creaba multiples marcadores

--- public/assets/app/crear.anuncio.js
+++ public/assets/app/crear.anuncio.js
@@ -1,24 +1,31 @@
 define(function () {
-    var geocoder = new google.maps.Geocoder();
-    var mapa = document.getElementById('mapa');
-    map  = new google.maps.Map( mapa, {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
-    });
-
+    var geocoder = new google.maps.Geocoder(),
+        mapa = document.getElementById('mapa'),
+        marker;
+        map  = new google.maps.Map( mapa, {
+            center: {lat: -34.397, lng: 150.644},
+            zoom: 8
+        });
     
     $(document).on('click', '#ubicar-direccion', function(e){
         e.preventDefault();
 
         geocoder.geocode({'address': $('input[name=direccion]').val()}, function(results, status) {
-            console.log(results)
+
             if (status === google.maps.GeocoderStatus.OK) {
                 map.setCenter(results[0].geometry.location);
                 map.setZoom(18);
-                var marker = new google.maps.Marker({
-                    map: map,
-                    position: results[0].geometry.location
-                });
+
+                if (marker) {
+                    marker.setPosition(results[0].geometry.location);
+                } else {
+                    marker = new google.maps.Marker({
+                        map: map,
+                        position: results[0].geometry.location,
+                        draggable: true
+                    });
+                }
+
             } else {
                 alert('Geocode was not successful for the following reason: ' + status);
             }
