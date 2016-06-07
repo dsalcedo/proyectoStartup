f16771c66e8457af23f86a14a56ecaf4a8e7fc37
danieluyo <dsalcedo@outlook.com>
2016-06-07 00:51:57 -0500

    #develop slideout

--- resources/views/webapp/v1/app.blade.php
+++ resources/views/webapp/v1/app.blade.php
@@ -15,7 +15,7 @@
     <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,700' rel='stylesheet' type='text/css'>
     {{ Html::style('assets/css/font-awesome.css') }}
-    {{ Html::style('assets/css/theme/app.css') }}
+    {{ Html::style('assets/css/theme/appV1.css') }}
 
     @yield('css')
 
@@ -25,22 +25,34 @@
 <!-- Static navbar -->
 <nav class="navbar navbar-default navbar-custom navbar-fixed-top">
     <div class="container-fluid">
+
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="{{route('website.index')}}">
-                <i class="middle fa fa-rocket fa-3x"></i>
+            <a href="#" id="toggle-button" class="toggle-button navbar-brand" >
+                <i class="fa fa-bars" aria-hidden="true"></i>
             </a>
+            <i class="fa fa-compass only-mobile" aria-hidden="true" style="font-size: 64px; margin-left: 25%; color: #0b97c4; margin-top:3px;"></i>
         </div>
+
+
+
         <div id="navbar" class="navbar-collapse collapse">
-            <ul class="nav navbar-nav navbar-right">
+            <div class="col-md-6">
+                <div class="row">
+                    <form class="navbar-form navbar-left full-width" role="search">
+                        <div class="input-group full-width">
+                            <span class="input-group-addon" style="color: #c4c4c4;">
+                                <i class="glyphicon glyphicon-search"></i>
+                            </span>
+                            <input type="text"  class="form-control" placeholder="Hacer una búsqueda" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <ul class="nav navbar-nav navbar-right ">
                 <li><a href="{{route('app.buscar')}}">Quiero buscar</a></li>
                 <li><a href="{{route('app.crearAnuncio')}}" style="background: #f6f037;">¡Crear un anuncio!</a></li>
             </ul>
+
         </div><!--/.nav-collapse -->
     </div>
 </nav>
@@ -48,7 +60,20 @@
 @yield('content')
 
 <script src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
+<script src="{{asset('assets/libs/slideout/js/slideout.min.js')}}"></script>
+<script>
+    var slideout = new Slideout({
+        'panel': document.getElementById('app-container'),
+        'menu': document.getElementById('sidebar'),
+        'padding': 256,
+        'tolerance': 76
+    });
+    document.querySelector('.toggle-button').addEventListener('click', function() {
+        slideout.toggle();
+    });
+</script>
 <script src="{{asset('assets/libs/jquery/jquery.min.js')}}"></script>
+<script src="{{asset('assets/libs/bootstrap/bootstrap.min.js')}}"></script>
 @yield('js')
 
 </body>
